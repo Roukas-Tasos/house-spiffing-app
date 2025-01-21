2789448d9dd97fb2ef8e3910d52e816923beb4a4
Tasos Roukas <tatsos7@gmail.com>
2025-01-21 20:37:55 +0200

    final auth commit

--- src/main/java/gr/housespiffingapp/security/SecurityConfiguration.java
+++ src/main/java/gr/housespiffingapp/security/SecurityConfiguration.java
@@ -46,10 +46,11 @@ public class SecurityConfiguration {
                 .exceptionHandling(exceptions -> exceptions
                         .authenticationEntryPoint(myCustomAuthenticationEntryPoint()))
                 .authorizeHttpRequests(req -> req
+                                .requestMatchers("/").permitAll()
                         .requestMatchers("api/auth/authenticate").permitAll()
-//                        .requestMatchers("api/users/**").authenticated()
-                        .requestMatchers("api/categories/**").permitAll()
-                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("api/categories/**").authenticated()
+                        .requestMatchers("api/chores/**").authenticated()
+//                        .requestMatchers("/**").permitAll()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider())
